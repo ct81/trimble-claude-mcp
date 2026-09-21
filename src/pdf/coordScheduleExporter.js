741e09edb12355de442532529e69e4316f7097c9
Chester Tan <124870488+ct81@users.noreply.github.com>
2026-09-21 06:25:46 +0000

    Start MCP, Swagger, UI, TC Workspace API and Property Set API #22

--- src/pdf/coordScheduleExporter.js
+++ src/pdf/coordScheduleExporter.js
@@ -1273,7 +1273,8 @@ function createDataSheet(sheet, items, workbook, activeColumns) {
             rowData.get("DetailMark") ||
             rowData.get("Mark") ||
             "";
-        const isDetailMarkRow = /^43[C|P]/.test(detailMarkValue);
+        //const isDetailMarkRow = /^43[C|P]/.test(detailMarkValue);
+        const isDetailMarkRow = /^\d{2}[A-Z]/i.test(detailMarkValue);
 
         if (isDetailMarkRow) {
             currentDetailMark = detailMarkValue;
