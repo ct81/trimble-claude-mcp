6aae374222436312014d3c3d3cf452971e389188
Chester Tan <124870488+ct81@users.noreply.github.com>
2026-09-21 06:17:24 +0000

    Start MCP, Swagger, UI, TC Workspace API and Property Set API #21

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 // git add . 
-// git commit -m "Start MCP, Swagger, UI, TC Workspace API and Property Set API #20"
+// git commit -m "Start MCP, Swagger, UI, TC Workspace API and Property Set API #21"
 // git push origin main
 
 import express from 'express';
